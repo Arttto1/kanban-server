488a80982e2986fd948795e3070c5913d0848831
Arthur Nunes <arthurnunes10@live.com>
2024-12-19 17:51:33 -0300

    mudança cancelada

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -15,6 +11,6 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Accept', 
   });
 
-  await app.listen(process.env.NODE_ENV ? "http://localhost:3000" : process.env.PORT, "0.0.0.0");
+  await app.listen(process.env.PORT, "0.0.0.0");
 }
 bootstrap();
