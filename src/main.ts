a2b755717d27867302bcdfe2b190a45c055d59fe
Arthur Nunes <arthurnunes10@live.com>
2024-12-19 17:46:46 -0300

    mudança do listen para testes locais

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Habilitar CORS
   app.enableCors({
-    origin: 'https://kanban-fieldcontrol.netlify.app',
+    origin: ['https://kanban-fieldcontrol.netlify.app', 'http://localhost:4200'],
     methods: 'GET,POST', 
     allowedHeaders: 'Content-Type, Accept', 
   });
 
-  await app.listen(process.env.PORT, "0.0.0.0");
+  await app.listen(process.env.NODE_ENV ? "http://localhost:3000" : process.env.PORT, "0.0.0.0");
 }
 bootstrap();
